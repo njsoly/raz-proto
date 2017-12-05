556873c51f2b0e350a753298b16d49cce9b35bec
Nate Solyntjes <njsoly@syntj.com>
2017-12-05 01:52:56 -0600

    getting keypress_control started.

---
+++ keypress_control.cpp
@@ -0,0 +1,18 @@
+/**************************************************************
+ **************      keypress_control.cpp       ***************
+ * this is source for a program that will take 				
+ * single-character keystroke commands until the quit 
+ * character 'q' (or 'Q' if CAPS) is entered.
+ */
+
+ #include <iostream>
+ //#include "raz_follower_defs.h"
+ 
+ using namespace std;
+ 
+ int main(){
+ 
+	cout << "hello, you" << endl;
+ 
+	return 0;
+ }
