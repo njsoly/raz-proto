5a47782240ad2380318303dec9f2d9ab6ca1b63e
njsoly <njsoly@syntj.com>
2017-12-27 13:49:59 -0600

    +commandLoop.cpp: for development of interactive command-line interface to include in main follower program.

---
+++ commandLoop.cpp
@@ -0,0 +1,19 @@
+/** commandLoop.cpp */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pigpio.h>
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "raz_follower_defs.h"
+
+using namespace std;
+
+
+
+int main() {
+	
+	
+}
