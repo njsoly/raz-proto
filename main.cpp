3dc9ada619db704585bf2716971d38548a131974
njsoly <njsoly@syntj.com>
2017-12-27 05:36:17 -0600

    main.cpp: lots of changes, changed sensor function to read one at a time.

--- main.cpp
+++ main.cpp
@@ -45,10 +45,10 @@
 
 using namespace std;
 
-long int rc_values[3]={0,0,0};
+uint32_t rc_values[3]={0,0,0};
 uint16_t rc_charge_us = 50;
 
-long int* qtr3rc_read(){
+uint32_t* qtr3rc_read_parallel(){
 	gpioSetPad(0, 16); // the sensors want 17mA, let's at least get 16 (the max).
 	// set the RC sensor pins to output, and set HIGH to charge
 	gpioSetMode(RC_2, PI_OUTPUT);
@@ -80,9 +80,71 @@ long int* qtr3rc_read(){
 		}
 	}
 	
-	rc_values[0] = (int) (ticks[0] - tickStart);
-	rc_values[1] = (int) (ticks[1] - tickStart);
-	rc_values[2] = (int) (ticks[2] - tickStart);
+	rc_values[0] = (ticks[0] - tickStart);
+	rc_values[1] = (ticks[1] - tickStart);
+	rc_values[2] = (ticks[2] - tickStart);
+	
+	
+	
+	return &(rc_values[0]);
+}
+/** this version of the method doesn't try to charge or read the sensors
+ * in a parallel fashion
+ */
+uint32_t* qtr3rc_read(){
+	uint32_t ticks[3]{0,0,0};
+	uint32_t tickStart = gpioTick();
+	
+	printf("tickStart: %u\n", (tickStart));
+	
+	gpioSetPad(0, 16); // the sensors want 17mA, let's at least get 16 (the max).
+	if(gpioGetPad(0) != 16) {
+		printf("ERROR: pad strength set didn't work.\n");
+	}
+	
+	gpioSetMode(RC_2, PI_INPUT);
+	gpioSetMode(RC_1, PI_INPUT);
+	gpioSetMode(RC_3, PI_INPUT);
+
+	// read sensor 1
+	tickStart = gpioTick();
+	gpioSetMode(RC_1, PI_OUTPUT);
+	gpioWrite(RC_1, 1);
+	gpioDelay(rc_charge_us);
+	gpioSetMode(RC_1, PI_INPUT);
+	while(ticks[0] == 0){
+		if((gpioRead(RC_1) == 0)){
+			ticks[0] = gpioTick();
+		}
+	}
+	rc_values[0] = (ticks[0] - tickStart);
+
+	// read sensor 2
+	tickStart = gpioTick();
+	gpioSetMode(RC_2, PI_OUTPUT);
+	gpioWrite(RC_2, 1);
+	gpioDelay(rc_charge_us);
+	gpioSetMode(RC_2, PI_INPUT);
+	while(ticks[2] == 0){
+		if(ticks[2] == 0 && (gpioRead(RC_3) == 0)){
+			ticks[2] = gpioTick();
+		}
+	}
+	rc_values[1] = (ticks[1] - tickStart);
+	
+	// read sensor 3
+	tickStart = gpioTick();
+	gpioSetMode(RC_3, PI_OUTPUT);
+	gpioWrite(RC_3, 1);
+	gpioDelay(rc_charge_us);
+	gpioSetMode(RC_3, PI_INPUT);
+	while(ticks[1] == 0){
+		if((gpioRead(RC_2) == 0)){
+			ticks[1] = gpioTick();
+		}
+	}
+	
+	rc_values[2] = (ticks[2] - tickStart);
 	
 	
 	
@@ -115,7 +177,7 @@ int main(){
 	cout << "reading sensors:  " << endl;
 	for(int i = 0; i < 100; i++){
 		qtr3rc_read();
-		printf("%lu\t%lu\t%lu\t   \n", rc_values[0], rc_values[1], rc_values[2] );
+		printf("%u\t%u\t%u\t   \n", rc_values[0], rc_values[1], rc_values[2] );
 		time_sleep(0.250f);
 	}
 
